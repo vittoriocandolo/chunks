7d31e26706ed363dc5704979c3e14d7a1b0091c1
vittoriocandolo <56765332+vittoriocandolo@users.noreply.github.com>
2022-10-18 17:50:41 +0200

    Fixed Nvidia max brightness issue

--- bash/setbri.sh
+++ bash/setbri.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "$1" > /sys/class/backlight/amdgpu_bl0/brightness || echo "$1" > /sys/class/backlight/amdgpu_bl1/brightness || true
-echo "$1" > /sys/class/backlight/nvidia_0/brightness
+# max value is 255
+amd=$(($1<256 ? $1 : 255))
+
+# max value is 100
+nvidia=$((($1*100)/255))
+
+echo $amd > /sys/class/backlight/amdgpu_bl0/brightness || echo amd > /sys/class/backlight/amdgpu_bl1/brightness || true
+
+echo $nvidia > /sys/class/backlight/nvidia_0/brightness
