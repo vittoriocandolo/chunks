02cec48f18e1a5756422f11a893d2e53d472e050
vittoriocandolo <56765332+vittoriocandolo@users.noreply.github.com>
2022-11-19 01:52:46 +0100

    Fix

--- bash/setbri.sh
+++ bash/setbri.sh
@@ -6,6 +6,6 @@ amd=$(($1<256 ? $1 : 255))
 # max value is 100
 nvidia=$((($1*100)/255))
 
-echo $amd > /sys/class/backlight/amdgpu_bl0/brightness || echo amd > /sys/class/backlight/amdgpu_bl1/brightness || true
+echo $amd > /sys/class/backlight/amdgpu_bl0/brightness || echo $amd > /sys/class/backlight/amdgpu_bl1/brightness || true
 
 echo $nvidia > /sys/class/backlight/nvidia_0/brightness
